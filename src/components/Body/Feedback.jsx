412c4d2b169ad67ea3c291d6cbb1fa2d0f3c8602
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2024-07-29 01:06:28 +0530

    bug : feadback is not completed

---
+++ src/components/Body/Feedback.jsx
@@ -0,0 +1,26 @@
+const Feedback = () => {
+  return (
+      <section>
+          <h1 className="text-[#0B2C4B]  text-center text-4xl font-semibold">
+              What our <span className="text-[#F89A36]">users</span> say about us
+          </h1>
+          <div className="bg-[#AFAFAF1A] h-[250px] w-[982px] flex flex-row justify-between px-4">
+              <div className="flex flex-row">
+                  <h1 className="">&quot;</h1>
+                  <p>This is one of the best Legal Tech platform I have ever used.
+                      This platform is the one stop solution that most startups should use for their
+                      legal and consultation purposes. Kudos to the brains behind this platform.
+                  </p>
+              </div>
+              <div>
+                  <div>
+                      <img></img>
+                  </div>
+                  <h3>Michael Siebel</h3>
+                  <p>CEO, Y COMBINATOR</p>
+              </div>
+          </div>
+    </section>
+  )
+}
+export default Feedback
